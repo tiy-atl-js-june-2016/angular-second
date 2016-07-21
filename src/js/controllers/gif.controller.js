b11447200ab6a802f016f206958eb376dcc98485
Tim Whitacre <tim@timw.co>
2016-07-21 09:29:18 -0400

    added validation

--- src/js/controllers/gif.controller.js
+++ src/js/controllers/gif.controller.js
@@ -1,6 +1,7 @@
 function GifController ($scope, SERVER, $http) {
 
   $scope.gifs = [];
+  $scope.error = {};
 
   init();
 
@@ -10,12 +11,29 @@ function GifController ($scope, SERVER, $http) {
     });
   }
 
+  $scope.validateName = (name) => {
+    $scope.error.name = (name) ? '' : 'Please provide a name for the Gif';
+  };
+
+  $scope.validateUrl = (url) => {
+
+    let invalidURL = !url.startsWith('http');
+
+    if (invalidURL) {
+      $scope.error.url = 'Please provide a proper URL (starts with http or https)';
+    } else if (!url) {
+      $scope.error.url = 'Please provide a url for the Gif';
+    } else {
+      $scope.error.url = '';
+    }
+  };
+
   $scope.addGif = (gif) => {
     $http.post(SERVER.URL, gif).then( (res) => {
       $scope.gifs.push(res.data);
       $scope.gif = {};
     });
-  }
+  };
 
   $scope.deleteMe = (id) => {
     $http.delete(SERVER.URL + id).then( (res) => {
